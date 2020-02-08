c2a6a73f2e719580ba1aec643af1a794c3b60ed5
wschwieder <47260266+wschwieder@users.noreply.github.com>
2020-02-08 15:24:05 -0500

    JS and Cert Adds
    Added Certs to this repo and continued some different development projects

---
+++ JS/js_sandbox/app.js
@@ -0,0 +1,27 @@
+// Types of Console commands
+// log, warning, error, time
+// console.log('Hello World');
+// console.log(123);
+// console.log(true)
+
+// // JS Datatypes
+// // var, let, const
+
+// INIT
+// var and let can be initialized 
+// const cannot
+
+// var name = 'John Doe';
+// console.log(name)
+
+// vars can include letters, numbers, _, $
+// Cannot start with number
+
+// We are going to use camel case for this course
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// CONST
+// Has to have a value and cannot be reassigned
